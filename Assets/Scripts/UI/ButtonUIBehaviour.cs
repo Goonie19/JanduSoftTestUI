c6334e68ab1ea35a469df7037e238f03523018bc
Goonie19 <antonio.espinosabarrios98@gmail.com>
2023-11-10 16:05:50 +0100

    buttons enter and exit anims added and behaviour added to buttons

---
+++ Assets/Scripts/UI/ButtonUIBehaviour.cs
@@ -0,0 +1,52 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonUIBehaviour : MonoBehaviour
+{
+
+    [Header("UI References")]
+    public Image ButtonIconImage;
+
+    [Header("Button Parameters")]
+    [Range(1f, 1.2f)]
+    public float FinalScaleWhenEntering;
+    [Range(0.001f, 0.2f)]
+    public float TimeToZoomButton;
+
+
+    private Sequence _iconSequence;
+
+
+    //Metodo que hace el efecto de zoom in del boton al entrar el cursor
+    public void EnterButton()
+    {
+        if (_iconSequence != null)
+            _iconSequence.Kill();
+
+        _iconSequence = DOTween.Sequence();
+        
+        _iconSequence.Append(transform.DOScale(FinalScaleWhenEntering, TimeToZoomButton));
+        
+        _iconSequence.Join(ButtonIconImage.transform.DOPunchRotation(Vector3.forward, 0.5f).SetLoops(-1));
+        _iconSequence.Join(ButtonIconImage.transform.DOScale(FinalScaleWhenEntering, TimeToZoomButton));
+
+        _iconSequence.Play();
+    }
+
+    //Metodo que hace el efecto de zoom out del boton al salir el cursor
+    public void ExitButton()
+    {
+        if (_iconSequence != null)
+            _iconSequence.Kill();
+
+        _iconSequence.Append(transform.DOScale(1, 0.1f));
+
+        _iconSequence.Join(ButtonIconImage.transform.DORotate(Vector3.zero, 0.01f));
+        _iconSequence.Join(ButtonIconImage.transform.DOScale(1f, 0.1f));
+
+        _iconSequence.Play();
+    }
+}
